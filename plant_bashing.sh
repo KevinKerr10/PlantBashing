fe5a5b10f99e4e89cccff1593d00b843549fe923
Kevin <kevinkhatae@gmail.com>
2025-04-28 15:15:48 -0500

    added yes and no features and name feature to plant_bashing.sh file

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-
+echo "Hello welcome to PlantBashing, what is your name?"
+read name
+echo "It's nice to meet you $name"
+echo "do you want to plant a seed? (answer yes or no)"
+read answer
+if [ "$answer" = "yes" ];then
+	echo "you have dug a hole and planted a seed the size of an almond"
+elif [ "$answer" != "yes" ];then
+	echo "what are you even doing here than you know this is a plant growing simulator right?"
+fi
+echo "In this world time moves much faster than in yours,a seccond in your world could be hours in this one!"
